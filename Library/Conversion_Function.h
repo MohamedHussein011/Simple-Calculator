385c0f833e4299c56880553666680a4c29f776a6
Mohamed Hussein <100514033+MohamedHussein011@users.noreply.github.com>
2022-08-26 18:05:04 +0200

    Create Conversion_Function.h

---
+++ Library/Conversion_Function.h
@@ -0,0 +1,15 @@
+/*
+ * Conversion_Function.h
+ *
+ *  	Created on: 24/08/2022
+ *      Author: Mohamed Hussein
+ *      Layer: Library
+ */
+
+#ifndef _CONVERSION_FUNCTION_H
+#define _CONVERSION_FUNCTION_H
+
+void ConvertToString(char* Copy_String, int32_t s32Copy_Number);
+int32_t ConvertToint(const char* Copy_String);
+
+#endif /* LIBRARY_CONVERSION_FUNCTION_H_ */
