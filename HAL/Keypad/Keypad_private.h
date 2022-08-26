be6194a2b6ccafa24c545b34200ac821042047b3
Mohamed Hussein <100514033+MohamedHussein011@users.noreply.github.com>
2022-08-26 18:07:07 +0200

    Create Keypad_private.h

---
+++ HAL/Keypad/Keypad_private.h
@@ -0,0 +1,15 @@
+/*
+ * KeyPad_private.h
+ *
+ *  	Created on: 24/08/2022
+ *      Author: Mohamed Hussein
+ *      Layer: HAL
+ */
+
+#ifndef _KEYPAD_PRIVATE_H
+#define _KEYPAD_PRIVATE_H
+
+#define COLUMN_NUM              4
+#define ROW_NUM                 4
+
+#endif /* ECUAL_KEYPAD_KEYPAD_PRIVATE_H_ */
