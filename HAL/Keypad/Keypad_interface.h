75b2ad0ede355c7891c7bb1e7583e376b6c82df6
Mohamed Hussein <100514033+MohamedHussein011@users.noreply.github.com>
2022-08-26 18:06:48 +0200

    Create Keypad_interface.h

---
+++ HAL/Keypad/Keypad_interface.h
@@ -0,0 +1,16 @@
+/*
+ * KeyPad_interface.h
+ *
+ * 	    Created on: 24/08/2022
+ *      Author: Mohamed Hussein
+ *      Layer: HAL
+ */
+
+#ifndef _KEYPAD_INTERFACE_H
+#define _KEYPAD_INTERFACE_H
+
+void vKeypad_Init(void);
+
+char u8Keypad_GetPressedKey (void);
+
+#endif /* ECUAL_KEYPAD_KEYPAD_INTERFACE_H_ */
